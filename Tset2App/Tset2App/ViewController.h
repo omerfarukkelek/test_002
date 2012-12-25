25857bfb9ebbc66d2311a422afc8d95db3a5ca5a
Detay <OmerFaruk@Detays-MacBook.local>
2012-12-25 10:04:45 +0200

    Tsest App Commit

---
+++ Tset2App/Tset2App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tset2App
+//
+//  Created by Detay on 12/25/12.
+//  Copyright (c) 2012 DetaySoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Tset2App/Tset2App/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  Tset2App
+//
+//  Created by Detay on 12/25/12.
+//  Copyright (c) 2012 DetaySoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
